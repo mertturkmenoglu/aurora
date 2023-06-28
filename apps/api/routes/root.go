427f9fd0e7a442614e5c391084b5a55658edc938
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-06-28 04:24:16 +0300

    Add temporary secret route to test auth middleware

--- apps/api/routes/root.go
+++ apps/api/routes/root.go
@@ -4,6 +4,7 @@ import (
 	"aurora/routes/auth"
 	"aurora/routes/aws"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Bootstrap(router *gin.Engine) {
@@ -14,4 +15,9 @@ func Bootstrap(router *gin.Engine) {
 	app.GET("/dynamo", aws.GetAwsDummyDynamoData)
 	app.POST("/auth/register", auth.RegisterUser)
 	app.POST("/auth/login", auth.LoginUser)
+	app.GET("/secret", auth.IsAuth(), func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Secret data",
+		})
+	})
 }
