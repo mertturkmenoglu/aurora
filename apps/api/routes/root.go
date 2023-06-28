b90f8daf1e5b3b9df4669415f5e1feabb5316fa3
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-06-28 04:51:30 +0300

    Use rate limiter middleware

--- apps/api/routes/root.go
+++ apps/api/routes/root.go
@@ -1,21 +1,28 @@
 package routes
 
 import (
+	"aurora/middlewares"
 	"aurora/routes/auth"
 	"aurora/routes/aws"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/ratelimit"
 	"net/http"
+	"time"
 )
 
 func Bootstrap(router *gin.Engine) {
 	app := router.Group("/v1")
+
+	middlewares.Limit = ratelimit.New(1000, ratelimit.Per(time.Minute))
+
+	app.Use(middlewares.LeakBucket())
 
 	app.GET("/test", GetTestData)
 	app.GET("/s3", aws.GetAwsDummyS3Data)
 	app.GET("/dynamo", aws.GetAwsDummyDynamoData)
 	app.POST("/auth/register", auth.RegisterUser)
 	app.POST("/auth/login", auth.LoginUser)
-	app.GET("/secret", auth.IsAuth(), func(c *gin.Context) {
+	app.GET("/secret", middlewares.IsAuth(), func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Secret data",
 		})
