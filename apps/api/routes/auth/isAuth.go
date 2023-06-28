8295166d78bd223c79d980e63f67759ff98fa196
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-06-28 04:22:36 +0300

    Add authentication middleware

---
+++ apps/api/routes/auth/isAuth.go
@@ -0,0 +1,35 @@
+package auth
+
+import (
+	"aurora/services/jwt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func IsAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		accessToken := c.GetHeader("x-access-token")
+
+		if accessToken == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Missing access token",
+			})
+			c.Abort()
+			return
+		}
+
+		_, err := jwt.DecodeJwt(accessToken)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+			c.Abort()
+			return
+		}
+
+		// TODO: Verify user
+
+		c.Next()
+	}
+}
