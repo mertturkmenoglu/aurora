7a80d31f18c3b9541fd4e39630335b4859078177
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-06-29 18:02:41 +0300

    Add user verification to auth middleware

--- apps/api/middlewares/auth.middleware.go
+++ apps/api/middlewares/auth.middleware.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	authmod "aurora/routes/auth"
 	"aurora/services/jwt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -18,7 +19,7 @@ func IsAuth() gin.HandlerFunc {
 			return
 		}
 
-		_, err := jwt.DecodeJwt(accessToken)
+		claims, err := jwt.DecodeJwt(accessToken)
 
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{
@@ -28,7 +29,32 @@ func IsAuth() gin.HandlerFunc {
 			return
 		}
 
-		// TODO: Verify user
+		var auth authmod.Auth
+		authResult, err := auth.GetByEmail(claims.Email)
+
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": err.Error(),
+			})
+			c.Abort()
+			return
+		}
+
+		if authResult.Email == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid credentials",
+			})
+			c.Abort()
+			return
+		}
+
+		reqUser := jwt.Payload{
+			Id:       authResult.Id,
+			FullName: authResult.FullName,
+			Email:    authResult.Email,
+		}
+
+		c.Set("user", reqUser)
 
 		c.Next()
 	}
