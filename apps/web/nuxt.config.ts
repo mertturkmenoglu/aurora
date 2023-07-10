a8ee7e0a6ceaac34b36b895fe2bb0d558bbc6afc
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-10 17:49:45 +0300

    Add i18n module

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-nocheck
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', ['@nuxtjs/eslint-module', { fix: true }]],
+    devtools: {enabled: true},
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/i18n',
+    ],
+    i18n: {
+        locales: ['en', 'tr'],
+        defaultLocale: 'en',
+        vueI18n: './i18n.config.ts',
+    },
 })
--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,3 +1,4 @@
 <template>
   <h1>This is the home page</h1>
+  <h2>{{ $t('welcome') }} </h2>
 </template>
