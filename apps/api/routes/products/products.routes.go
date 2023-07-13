7b1cbd304cb7b1b6d7159474a709a7c39345e65a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-13 13:04:25 +0300

    Refactor products routes

--- apps/api/routes/products/products.routes.go
+++ apps/api/routes/products/products.routes.go
@@ -3,7 +3,7 @@ package products
 import (
 	"aurora/services/aws/models"
 	"aurora/services/cache"
-	"encoding/json"
+	"aurora/services/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -12,64 +12,24 @@ func GetProductById(c *gin.Context) {
 	id := c.Param("id")
 	var product *models.Product
 
-	// Check cache
-	cacheResult, err := cache.HGetAll(cache.ProductKey(id))
-
-	// Cache hit
-	if err == nil && len(cacheResult) > 0 {
-		err = json.Unmarshal([]byte(cacheResult["data"]), &product)
-
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{
-				"message": err.Error(),
-			})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"data": product,
-		})
-		return
-	}
+	utils.CheckCache[models.Product](c, cache.ProductKey(id))
 
 	// Cache miss
 	productResult, err := product.GetProductById(id)
 
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": err.Error(),
-		})
+		utils.ErrorResponse(c, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	// Check if product exists
 	if productResult.Id == "" {
-		c.JSON(http.StatusNotFound, gin.H{
-			"message": "Product not found",
-		})
+		utils.ErrorResponse(c, http.StatusNotFound, "Product not found")
 		return
 	}
 
 	// Set cache
-	serializedProduct, err := json.Marshal(productResult)
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
-
-	err = cache.HSet(cache.ProductKey(id), map[string]string{
-		"data": string(serializedProduct),
-	})
-
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": err.Error(),
-		})
-		return
-	}
+	utils.SetCache[models.Product](c, cache.ProductKey(id), productResult)
 
 	c.JSON(http.StatusOK, gin.H{
 		"data": productResult,
--- apps/api/services/utils/gin-utils.go
+++ apps/api/services/utils/gin-utils.go
@@ -1,11 +1,38 @@
 package utils
 
 import (
+	"aurora/services/aws/models"
+	"aurora/services/cache"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func ErrorResponse(c *gin.Context, status int, errorMessage string) {
 	c.JSON(status, gin.H{
 		"error": errorMessage,
 	})
+}
+
+func CheckCache[T models.DynamoModel](c *gin.Context, key string) {
+	var result *T
+
+	cacheResult, err := cache.HGetAll(key)
+
+	// Cache hit
+	if err == nil && len(cacheResult) > 0 {
+		err = json.Unmarshal([]byte(cacheResult["data"]), &result)
+
+		if err != nil {
+			ErrorResponse(c, http.StatusInternalServerError, err.Error())
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": result,
+		})
+		return
+	}
+
+	// Cache miss, continue to regular flow
 }
