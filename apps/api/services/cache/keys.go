3c53cb06f8edfc1e14719bdadd7822b683a26006
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-13 13:10:01 +0300

    Add new cache key

--- apps/api/services/cache/keys.go
+++ apps/api/services/cache/keys.go
@@ -4,8 +4,13 @@ import "fmt"
 
 const (
 	ProductKeyFormat = "product:%s"
+	UserKeyFormat    = "user:%s"
 )
 
 func ProductKey(id string) string {
 	return fmt.Sprintf(ProductKeyFormat, id)
+}
+
+func UserKey(email string) string {
+	return fmt.Sprintf(UserKeyFormat, email)
 }
