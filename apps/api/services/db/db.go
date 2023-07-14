60b0b610c2841d473106cf1ca6913a4e59a6ac7e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-15 02:03:13 +0300

    Create database module

---
+++ apps/api/services/db/db.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"aurora/services/db/models"
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+var Client *gorm.DB
+
+func Init() {
+	host, hostOk := os.LookupEnv("DB_HOST")
+	user, userOk := os.LookupEnv("DB_USER")
+	password, passwordOk := os.LookupEnv("DB_PASSWORD")
+	dbname, dbnameOk := os.LookupEnv("DB_NAME")
+	port, portOk := os.LookupEnv("DB_PORT")
+	timezone, timezoneOk := os.LookupEnv("DB_TIMEZONE")
+
+	if !hostOk || !userOk || !passwordOk || !dbnameOk || !portOk || !timezoneOk {
+		panic("Missing database environment variables!")
+	}
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=%s", host, user, password, dbname, port, timezone)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	Client = db
+}
+
+func AutoMigrate() error {
+	return Client.AutoMigrate(&models.Auth{}, &models.Test{}, &models.User{}, &models.Brand{}, &models.Product{})
+}
