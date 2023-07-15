1bc0f0ca33f1ffd75b97e291583b575651ff1c6d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-15 12:53:25 +0300

    Add new utility functions

---
+++ apps/api/services/utils/auth.go
@@ -0,0 +1,47 @@
+package utils
+
+import (
+	"aurora/services/db"
+	"aurora/services/db/models"
+	"github.com/google/uuid"
+	"strings"
+	"unicode"
+)
+
+func DoesUserExist(email string) (bool, error) {
+	var auth models.Auth
+	res := db.Client.First(&auth, "email = ?", email)
+
+	if res.Error != nil {
+		if strings.Contains(res.Error.Error(), "record not found") {
+			return false, nil
+		}
+		return false, res.Error
+	}
+
+	return true, nil
+}
+
+func CustomPasswordCheck(password string) bool {
+	hasUpper := false
+	hasLower := false
+
+	for _, char := range password {
+		if unicode.IsUpper(char) {
+			hasUpper = true
+		}
+		if unicode.IsLower(char) {
+			hasLower = true
+		}
+		if hasLower && hasUpper {
+			break
+		}
+	}
+
+	return hasUpper && hasLower
+}
+
+func GenerateRandomShortId() string {
+	sid := uuid.NewString()
+	return strings.Split(sid, "-")[0]
+}
--- apps/api/services/utils/gin-utils.go
+++ apps/api/services/utils/gin-utils.go
@@ -2,10 +2,27 @@ package utils
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
 func ErrorResponse(c *gin.Context, status int, errorMessage string) {
 	c.JSON(status, gin.H{
 		"error": errorMessage,
 	})
+}
+
+func isNotFoundError(err error) bool {
+	return strings.Contains(err.Error(), "record not found")
+}
+
+func HandleDatabaseError(c *gin.Context, err error) {
+	if isNotFoundError(err) {
+		ErrorResponse(c, http.StatusNotFound, "record not found")
+		c.Abort()
+		return
+	}
+
+	ErrorResponse(c, http.StatusInternalServerError, err.Error())
+	c.Abort()
 }
