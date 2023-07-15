c9836c84489f233b77a521bafb0132ff3947d938
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-15 23:27:24 +0300

    Add create product route and update get product route

--- apps/api/handlers/products.go
+++ apps/api/handlers/products.go
@@ -1,16 +1,79 @@
 package handlers
 
 import (
+	"aurora/handlers/dto"
 	"aurora/services/cache"
 	"aurora/services/db"
 	"aurora/services/db/models"
 	"aurora/services/utils"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"net/http"
 )
 
+func CreateProduct(c *gin.Context) {
+	body := c.MustGet("body").(dto.CreateProductDto)
+
+	brandIdAsUUID, err := uuid.Parse(body.BrandId)
+
+	if err != nil {
+		utils.ErrorResponse(c, http.StatusBadRequest, "brandId is missing or malformed")
+		return
+	}
+
+	product := &models.Product{
+		Name:          body.Name,
+		Description:   body.Description,
+		CurrentPrice:  body.CurrentPrice,
+		OldPrice:      body.OldPrice,
+		Inventory:     body.Inventory,
+		IsFeatured:    body.IsFeatured,
+		IsNew:         body.IsNew,
+		IsOnSale:      body.IsOnSale,
+		IsPopular:     body.IsPopular,
+		ShippingPrice: body.ShippingPrice,
+		ShippingTime:  body.ShippingTime,
+		ShippingType:  body.ShippingType,
+		Slug:          body.Slug,
+		BrandId:       brandIdAsUUID,
+		Images:        []models.ProductImage{},
+	}
+
+	res := db.Client.Create(product)
+
+	if res.Error != nil {
+		utils.HandleDatabaseError(c, res.Error)
+		return
+	}
+
+	images := make([]*models.ProductImage, len(body.Images))
+
+	for i, image := range body.Images {
+		images[i] = &models.ProductImage{
+			ProductId: product.Id,
+			Url:       image.Url,
+		}
+	}
+
+	res = db.Client.Create(&images)
+
+	if res.Error != nil {
+		utils.HandleDatabaseError(c, res.Error)
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"data": product,
+	})
+}
+
 func GetProductById(c *gin.Context) {
 	id := c.Param("id")
+
+	if _, err := uuid.Parse(id); err != nil {
+		utils.ErrorResponse(c, http.StatusBadRequest, "id is malformed")
+		return
+	}
 
 	key := cache.GetFormattedKey(cache.ProductKeyFormat, id)
 	cacheResult, err := cache.HGet[models.Product](key)
@@ -23,7 +86,10 @@ func GetProductById(c *gin.Context) {
 	}
 
 	var product *models.Product
-	res := db.Client.First(&product, "ID = ?", id)
+	res := db.Client.
+		Preload("Images").
+		Preload("Brand").
+		First(&product, "ID = ?", id)
 
 	if res.Error != nil {
 		utils.HandleDatabaseError(c, res.Error)
