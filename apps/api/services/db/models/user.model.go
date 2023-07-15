6178a618e4c6ecc643be7a00ebae3d8308442125
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-15 23:26:31 +0300

    Update user model JSON field names

--- apps/api/services/db/models/user.model.go
+++ apps/api/services/db/models/user.model.go
@@ -4,32 +4,32 @@ import "github.com/google/uuid"
 
 type User struct {
 	BaseModel
-	FullName     string
-	Email        string
-	Phone        string
-	AdPreference AdPreference
-	Addresses    []Address
+	FullName     string       `json:"fullName"`
+	Email        string       `json:"email"`
+	Phone        string       `json:"phone"`
+	AdPreference AdPreference `json:"adPreference"`
+	Addresses    []Address    `json:"addresses"`
 }
 
 type AdPreference struct {
 	BaseModel
-	UserID uuid.UUID
-	Email  bool
-	Sms    bool
-	Phone  bool
+	UserID uuid.UUID `json:"userId"`
+	Email  bool      `json:"email"`
+	Sms    bool      `json:"sms"`
+	Phone  bool      `json:"phone"`
 }
 
 type Address struct {
 	BaseModel
-	UserID      uuid.UUID
-	City        string
-	Description string
-	IsDefault   bool
-	Line1       string
-	Line2       string
-	Name        string
-	Phone       string
-	State       string
-	Type        string
-	ZipCode     string
+	UserID      uuid.UUID `json:"userId"`
+	City        string    `json:"city"`
+	Description string    `json:"description"`
+	IsDefault   bool      `json:"isDefault"`
+	Line1       string    `json:"line1"`
+	Line2       string    `json:"line2"`
+	Name        string    `json:"name"`
+	Phone       string    `json:"phone"`
+	State       string    `json:"state"`
+	Type        string    `json:"type"`
+	ZipCode     string    `json:"zipCode"`
 }
