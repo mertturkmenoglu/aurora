a9aeed90da5324f44dfb1585fc95208710cb2311
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-19 01:47:24 +0300

    Delete dummy homepage content

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,4 +1,2 @@
 <template>
-  <h2>{{ $t('home.welcomeText') }} </h2>
-  <nuxt-img src="https://fastly.picsum.photos/id/58/1280/853.jpg?hmac=YO3QnOm9TpyM5DqsJjoM4CHg8oIq4cMWLpd9ALoP908"/>
 </template>
