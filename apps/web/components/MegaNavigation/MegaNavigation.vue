f08491e2a7622e419e97194ae8b41531c234364e
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-19 01:48:19 +0300

    Add MegaNavigation component

---
+++ apps/web/components/MegaNavigation/MegaNavigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <div class="w-full flex items-center justify-between">
+      <div v-for="l0 in megaNavigationItems.items" class="flex">
+        <MegaNavigationItem :item="l0"/>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+import {megaNavigationItems} from './items'
+</script>
