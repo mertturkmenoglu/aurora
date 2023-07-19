374857344c612a51af61a29699785a4cef11aa2a
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-19 07:30:46 +0300

    Add data fetching to MegaNavigation

--- apps/web/components/MegaNavigation/MegaNavigation.vue
+++ apps/web/components/MegaNavigation/MegaNavigation.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="border-b border-midnight">
     <div class="w-full flex items-center justify-between">
-      <PopoverGroup v-for="l0 in megaNavigationItems.items" class="flex relative">
+      <PopoverGroup v-for="l0 in items.items" class="flex relative">
         <MegaNavigationItem :item="l0"/>
       </PopoverGroup>
     </div>
@@ -9,6 +9,25 @@
 </template>
 
 <script lang="ts" setup>
-import {megaNavigationItems} from './items'
+import {MegaNavigationItems} from './items'
 import {PopoverGroup} from "@headlessui/vue";
+
+const items = ref<MegaNavigationItems>({
+  items: []
+})
+
+const serialized = sessionStorage.getItem("megaNavigation")
+
+if (serialized) {
+  items.value = JSON.parse(serialized)
+} else {
+  const {data} = await useFetch<{ data: MegaNavigationItems }>("http://localhost:5000/api/v1/categories")
+
+  if (data.value) {
+    items.value = data.value.data
+    sessionStorage.setItem("megaNavigation", JSON.stringify(data.value.data))
+  }
+}
+
+
 </script>
