a3ffdc3ae22b20e2a1f24e27cc2751a3a044153f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-20 23:14:42 +0300

    Add new dtos

--- apps/web/utils/dto.ts
+++ apps/web/utils/dto.ts
@@ -6,3 +6,35 @@ export type LoginResponseDto = Dto<{
     accessToken: string
     refreshToken: string
 }>
+
+export type UserDto = Dto<User>
+
+export type User = {
+    id: string;
+    fullName: string;
+    email: string;
+    phone: string;
+    adPreference: AdPreference;
+    addresses: Address[];
+};
+
+export type AdPreference = {
+    id: string;
+    email: boolean;
+    sms: boolean;
+    phone: boolean;
+}
+
+export type Address = {
+    id: string;
+    city: string;
+    description: string;
+    isDefault: boolean;
+    line1: string;
+    line2: string;
+    name: string;
+    phone: string;
+    state: string;
+    type: string;
+    zipCode: string;
+}
