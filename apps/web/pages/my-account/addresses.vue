25ed44ef5139c99a32d07793d4bdf879229870a7
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-21 07:30:05 +0300

    Add addresses page

---
+++ apps/web/pages/my-account/addresses.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="isAuthenticated && user" class="container mx-auto my-16">
+    <nuxt-link class="flex items-center text-sky-600 text-sm hover:underline" to="/my-account">
+      <ChevronLeftIcon class="w-6 h-6 mr-2"/>
+      <span class="font-semibold">Back to My Account</span>
+    </nuxt-link>
+
+    <div class="mt-8 mb-64">
+      <h2 class="text-2xl text-sky-600 font-bold">Your Addresses</h2>
+
+      <div v-for="address in user.addresses" v-if="user.addresses.length > 0">
+        <div class="mt-4 space-y-2">
+          <pre class="flex space-x-2">
+            {{ JSON.stringify(address, null, 2) }}
+          </pre>
+        </div>
+      </div>
+      <div v-else class="mt-8">
+        You don't have any addresses
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ChevronLeftIcon} from "@heroicons/vue/24/outline";
+
+const {isAuthenticated, user} = useAuth()
+</script>
