d1ca4cc7d6735e1f6d5c0c40c183063a478eae2f
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-21 06:05:26 +0300

    Add product carousels to homepage

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -1,4 +1,28 @@
 <template>
+
+  <div class="container space-y-16 mx-auto">
+    <ProductCarousel
+        :items="featuredProducts as Product[]"
+        title="Featured products"
+    />
+
+    <ProductCarousel
+        :items="popularProducts as Product[]"
+        title="Popular products"
+    />
+
+    <ProductCarousel
+        :items="newProducts as Product[]"
+        title="New products"
+    />
+
+    <ProductCarousel
+        :items="freeShippingProducts as Product[]"
+        title="Free Shipping"
+    />
+
+  </div>
+
   <div v-if="products" class="container gap-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto xl:px-32 my-16">
     <div v-for="product in products" :key="product.id">
       <ProductCard :hoverable="true" :product="product"/>
@@ -8,10 +32,35 @@
 
 <script lang="ts" setup>
 import {ProductsDto, Product} from "~/utils/dto";
+import {BASE_URL} from "~/utils/api";
 
 const {
   data,
 } = await useFetch<ProductsDto>(`${BASE_URL}/products/all`);
+
+const {
+  data: featuredProductsData,
+} = await useFetch<ProductsDto>(`${BASE_URL}/products/featured`);
+
+const {
+  data: popularProductsData,
+} = await useFetch<ProductsDto>(`${BASE_URL}/products/popular`);
+
+const {
+  data: newProductsData,
+} = await useFetch<ProductsDto>(`${BASE_URL}/products/new`);
+
+const {
+  data: freeShippingProductsData,
+} = await useFetch<ProductsDto>(`${BASE_URL}/products/free-shipping`);
+
+const featuredProducts = featuredProductsData.value?.data ?? [];
+
+const popularProducts = popularProductsData.value?.data ?? [];
+
+const newProducts = newProductsData.value?.data ?? [];
+
+const freeShippingProducts = freeShippingProductsData.value?.data ?? [];
 
 const products: Product[] = data.value?.data || [];
 </script>
