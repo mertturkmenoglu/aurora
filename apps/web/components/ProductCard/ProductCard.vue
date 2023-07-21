ca8cd3d773ea6ba295fcd94fd6adc801ff4a3c26
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-21 05:53:35 +0300

    Update ProductCard

--- apps/web/components/ProductCard/ProductCard.vue
+++ apps/web/components/ProductCard/ProductCard.vue
@@ -1,13 +1,20 @@
 <template>
-  <nuxt-link :to="`/products/${product.id}`"
-             class="hover:border hover:border-sky-600 p-4 flex flex-col rounded-lg hover:scale-[102%] transition-all ease-in duration-200 group">
+  <nuxt-link
+      :class="clsx(
+          {
+          'hover:scale-[102%] transition-all ease-in duration-200 hover:border hover:border-sky-600': hoverable,
+          },
+          ' p-4 flex flex-col rounded-lg group'
+      )"
+      :to="`/products/${product.id}`"
+  >
     <div class="flex items-center justify-between">
       <span class="font-bold text-sm text-sky-600">{{ productMessage }}</span>
       <button>
         <HeartIcon class="w-7 h-6 text-gray-600 cursor-pointer"/>
       </button>
     </div>
-    <img :src="product.images[0]?.url ?? ''" alt="" class="w-64 h-48 sm:w-64 md:h-96 object-cover mt-2 mx-auto">
+    <img :src="image" alt="" class="w-64 h-48 sm:w-64 md:h-96 object-cover mt-2 mx-auto">
     <div class="mt-2">
       <span class="font-bold text-green-600">{{ product.currentPrice }}$</span>
       <span v-if="product.currentPrice !== product.oldPrice" class="font-light line-through ml-2">
@@ -19,7 +26,13 @@
     <span class="font-light text-sm mt-2">By {{ product.brand.name }}</span>
 
     <button
-        class="hidden group-hover:flex px-4 py-2 text-white bg-sky-600 rounded-full text-center justify-center mt-4">
+        :class="clsx(
+            {
+              'hidden group-hover:flex': hoverable,
+              'flex': !hoverable,
+            },
+            'px-4 py-2 text-white bg-sky-600 rounded-full text-center justify-center mt-4'
+        )">
       Add to cart
     </button>
   </nuxt-link>
@@ -30,10 +43,21 @@
 import {Product} from "~/utils/dto";
 import {HeartIcon} from "@heroicons/vue/24/outline"
 import {useProductMessage} from "~/composables/useProductMessage";
+import clsx from "clsx";
 
-const {product} = defineProps<{
+const {product, hoverable = true} = defineProps<{
   product: Product
+  hoverable?: boolean
 }>();
 
 const productMessage = useProductMessage(product);
+
+const image = computed(() => {
+  console.log(product)
+  if (!product.images.length) {
+    return ''
+  }
+
+  return product.images[0]?.url ?? ''
+})
 </script>
