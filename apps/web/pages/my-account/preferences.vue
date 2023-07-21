caf80008a7f465d9f6cc0fdaa1dcb9e94bd5f9ea
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-21 07:29:59 +0300

    Add account preferences page

---
+++ apps/web/pages/my-account/preferences.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="isAuthenticated && user" class="container mx-auto my-16">
+    <nuxt-link class="flex items-center text-sky-600 text-sm hover:underline" to="/my-account">
+      <ChevronLeftIcon class="w-6 h-6 mr-2"/>
+      <span class="font-semibold">Back to My Account</span>
+    </nuxt-link>
+
+    <div class="mt-8 mb-64">
+      <h2 class="text-2xl text-sky-600 font-bold">Your Preferences</h2>
+
+      <div class="mt-4 space-y-2">
+        <div class="flex space-x-2">
+          <div class="text-black font-bold">Email:</div>
+          <div>{{ user.adPreference.email }}</div>
+        </div>
+
+        <div class="flex space-x-2">
+          <div class="text-black font-bold">SMS:</div>
+          <div>{{ user.adPreference.sms }}</div>
+        </div>
+
+        <div class="flex space-x-2">
+          <div class="text-black font-bold">Phone:</div>
+          <div>{{ user.adPreference.phone }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {ChevronLeftIcon} from "@heroicons/vue/24/outline";
+
+const {isAuthenticated, user} = useAuth()
+</script>
