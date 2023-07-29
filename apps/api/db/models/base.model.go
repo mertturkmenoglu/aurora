ff3208a33e80cf7226bf2f42cf2cf30f8c8c20f5
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-29 15:52:41 +0300

    Exclude deletedAt field from JSON responses

--- apps/api/db/models/base.model.go
+++ apps/api/db/models/base.model.go
@@ -9,5 +9,5 @@ type BaseModel struct {
 	Id        uuid.UUID  `json:"id" gorm:"type:uuid;default:uuid_generate_v4()"`
 	CreatedAt time.Time  `json:"createdAt"`
 	UpdatedAt time.Time  `json:"updatedAt"`
-	DeletedAt *time.Time `json:"deletedAt" sql:"index"`
+	DeletedAt *time.Time `json:"-" sql:"index"`
 }
