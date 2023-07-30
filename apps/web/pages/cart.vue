6651689e61fb444743a476a30f8fb8eae93ea9dd
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-07-30 14:44:59 +0300

    Update Cart page

--- apps/web/pages/cart.vue
+++ apps/web/pages/cart.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="container mx-auto mt-16">
+  <div v-if="isAuthenticated && user" class="container mx-auto mt-16">
     <h2 class="text-2xl font-bold">Your Shopping Cart</h2>
+    <div v-if="cart" class="mt-16">
+      <div v-for="item in cart.items">
+        <CartItem :item="item"/>
+      </div>
+    </div>
   </div>
 </template>
+
+<script lang="ts" setup>
+import {Cart, CartDto} from "~/utils/dto";
+
+const {isAuthenticated, user} = useAuth()
+
+const {data} = await useFetch<CartDto>(`${BASE_URL}/cart`, {
+  headers: {
+    'x-access-token': localStorage.getItem('accessToken') || '',
+    'x-refresh-token': localStorage.getItem('refreshToken') || '',
+  },
+})
+
+const cart: Cart | undefined = data.value?.data
+</script>
