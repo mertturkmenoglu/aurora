70490d94c7befd3782e7dfab6abcbb0856d1d163
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-01 13:36:11 +0300

    Add product message hook

---
+++ apps/admin/src/hooks/useProductMessage.tsx
@@ -0,0 +1,15 @@
+import { Product } from '@/lib/api';
+
+export function useProductMessage(product: Product) {
+  if (product.shippingPrice === 0) {
+    return 'Free Shipping';
+  } else if (product.isPopular) {
+    return 'Popular';
+  } else if (product.isNew) {
+    return 'New';
+  } else if (product.isOnSale) {
+    return 'On Sale';
+  } else {
+    return '';
+  }
+}
