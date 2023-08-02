78a928e35403360e367fd76f1ab641855b82ac74
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-02 14:18:16 +0300

    Update dtos

--- apps/web/utils/dto.ts
+++ apps/web/utils/dto.ts
@@ -1,124 +1,135 @@
 export type Dto<T> = {
-    data: T
+  data: T
 }
 
 export type LoginResponseDto = Dto<{
-    accessToken: string
-    refreshToken: string
+  accessToken: string
+  refreshToken: string
 }>
 
 export type UserDto = Dto<User>
 
 export type User = {
-    id: string;
-    fullName: string;
-    email: string;
-    phone: string;
-    adPreference: AdPreference;
-    addresses: Address[];
+  id: string;
+  fullName: string;
+  email: string;
+  phone: string;
+  adPreference: AdPreference;
+  addresses: Address[];
 };
 
 export type AdPreference = {
-    id: string;
-    email: boolean;
-    sms: boolean;
-    phone: boolean;
+  id: string;
+  email: boolean;
+  sms: boolean;
+  phone: boolean;
 }
 
 export type Address = {
-    id: string;
-    city: string;
-    description: string;
-    isDefault: boolean;
-    line1: string;
-    line2: string;
-    name: string;
-    phone: string;
-    state: string;
-    type: string;
-    zipCode: string;
+  id: string;
+  city: string;
+  description: string;
+  isDefault: boolean;
+  line1: string;
+  line2: string;
+  name: string;
+  phone: string;
+  state: string;
+  type: string;
+  zipCode: string;
 }
 
 export type ProductsDto = Dto<Product[]>
 export type ProductDto = Dto<Product>
 
 export type Product = {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    name: string;
-    description: string;
-    currentPrice: number;
-    oldPrice: number;
-    inventory: number;
-    images: ProductImage[];
-    isFeatured: boolean;
-    isNew: boolean;
-    isOnSale: boolean;
-    isPopular: boolean;
-    shippingPrice: number;
-    shippingTime: string;
-    shippingType: string;
-    slug: string;
-    brandId: string;
-    brand: Brand;
-    categoryId: string;
-    category: Category;
-    styles: ProductStyle[];
-    sizes: ProductSize[];
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  description: string;
+  isFeatured: boolean;
+  isNew: boolean;
+  isOnSale: boolean;
+  isPopular: boolean;
+  brandId: string;
+  brand: Brand;
+  categoryId: string;
+  category: Category;
+  defaultVariantId: string;
+  defaultVariant: ProductVariant;
+  variants: ProductVariant[];
+}
+
+export type ProductVariant = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  productId: string;
+  currentPrice: number;
+  oldPrice: number;
+  inventory: number;
+  imageId: string;
+  image: ProductImage;
+  shippingPrice: number;
+  shippingTime: string;
+  shippingType: string;
+  styleId: string;
+  style: ProductStyle;
+  sizeId: string;
+  size: ProductSize;
 }
 
 export type ProductImage = {
-    id: string;
-    productId: string;
-    url: string;
+  id: string;
+  productId: string;
+  productVariantId: string;
+  url: string;
 }
 
 export type ProductStyle = {
-    id: string;
-    productId: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
 export type ProductSize = {
-    id: string;
-    productId: string;
-    name: string;
+  id: string;
+  name: string;
 }
 
 export type Brand = {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    name: string;
-    description: string;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  description: string;
 }
 
 export type Category = {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    name: string;
-    parentId: string | null;
-    parent: Category | null;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  parentId: string | null;
+  parent: Category | null;
 }
 
 export type Cart = {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    userId: string;
-    items: CartItem[];
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  userId: string;
+  items: CartItem[];
 }
 
 export type CartItem = {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    cartId: string;
-    productId: string;
-    product: Product;
-    quantity: number;
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  cartId: string;
+  productId: string;
+  product: Product;
+  quantity: number;
 }
 
 export type CartDto = Dto<Cart>
