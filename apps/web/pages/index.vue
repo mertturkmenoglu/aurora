340164b2c5ececa297c70c58d6163dc86eae4a5d
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-02 14:19:22 +0300

    Update homepage

--- apps/web/pages/index.vue
+++ apps/web/pages/index.vue
@@ -3,43 +3,38 @@
     <title>Aurora / Home</title>
   </Head>
 
-  <div class="container space-y-16 mx-auto">
+  <div class='container space-y-16 mx-auto'>
     <ProductCarousel
-        :items="featuredProducts as Product[]"
-        title="Featured products"
+      :items='featuredProducts as Product[]'
+      title='Featured products'
     />
 
     <ProductCarousel
-        :items="popularProducts as Product[]"
-        title="Popular products"
+      :items='popularProducts as Product[]'
+      title='Popular products'
     />
 
     <ProductCarousel
-        :items="newProducts as Product[]"
-        title="New products"
-    />
-
-    <ProductCarousel
-        :items="freeShippingProducts as Product[]"
-        title="Free Shipping"
+      :items='newProducts as Product[]'
+      title='New products'
     />
 
   </div>
 
-  <div v-if="products" class="container gap-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto xl:px-32 my-16">
-    <div v-for="product in products" :key="product.id">
-      <ProductCard :hoverable="true" :product="product"/>
+  <div v-if='products' class='container gap-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mx-auto xl:px-32 my-16'>
+    <div v-for='product in products' :key='product.id'>
+      <ProductCard :hoverable='true' :product='product' />
     </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-import {ProductsDto, Product} from "~/utils/dto";
-import {BASE_URL} from "~/utils/api";
+<script lang='ts' setup>
+import { ProductsDto, Product } from '~/utils/dto';
+import { BASE_URL } from '~/utils/api';
 
 const {
   data,
-} = await useFetch<ProductsDto>(`${BASE_URL}/products/all`);
+} = await useFetch<ProductsDto>(`${BASE_URL}/products/all?page=1&pageSize=90`);
 
 const {
   data: featuredProductsData,
