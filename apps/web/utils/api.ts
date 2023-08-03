cdb7888bdbd94aba6b29b72adfeadbc9cc3b3095
Mert Turkmenoglu <mertturkmenoglu99@gmail.com>
2023-08-03 14:49:46 +0300

    Reformat file

--- apps/web/utils/api.ts
+++ apps/web/utils/api.ts
@@ -1,7 +1,7 @@
-import {ofetch} from 'ofetch';
+import { ofetch } from 'ofetch';
 
-export const BASE_URL = 'http://localhost:5000/api/v1'
+export const BASE_URL = 'http://localhost:5000/api/v1';
 
 export const api = ofetch.create({
-    baseURL: BASE_URL,
-})
+  baseURL: BASE_URL,
+});
